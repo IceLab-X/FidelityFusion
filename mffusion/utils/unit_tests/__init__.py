2098601b2000ebe8edf3a2a44b01f0419d2f15f9
zen <739934831@qq.com>
2023-12-09 13:11:56 +0800

    remove previous version file

--- mffusion/utils/unit_tests/__init__.py
+++
@@ -1,9 +0,0 @@
-#   coding:utf-8
-#   This file is part of MF_Fusion.
-
-__author__ = 'Guanjie Wang'
-__email__ = "gjwang.buaa@gmail.edu.cn"
-__version__ = 1.0
-__init_date__ = '2023/06/15 09:53:20'
-__maintainer__ = 'Guanjie Wang'
-__update_date__ = '2023/06/15 09:53:20'
